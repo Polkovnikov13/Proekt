3be6d06928f5c51d4fbf1e427bcb7de47f1bdb2c
Artem Polkovnikov <artem.polkovnikov93@yandex.ru>
2023-04-20 14:16:25 +0300

    Navbar done

--- frontend/src/MyNavbar/MyNavbar.jsx
+++ frontend/src/MyNavbar/MyNavbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './MyNavbar.css';
 import {
   Collapse,
   Navbar,
@@ -7,32 +8,59 @@ import {
   Nav,
   NavItem,
   NavLink,
-
-  NavbarText,
+  Input,
+  CardImg,
+  Button,
 } from 'reactstrap';
 
 export default function MyNavbar() {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
   return (
-
     <Navbar>
-      <NavbarToggler onClick={toggle} />
-      <NavbarBrand href="/">reactstrap</NavbarBrand>
-      <NavbarBrand href="/">reactstrap</NavbarBrand>
-      <NavbarBrand href="/">reactstrap</NavbarBrand>
-      <input />
-      <NavbarBrand href="/">reactstrap</NavbarBrand>
-      <Collapse isOpen={isOpen} navbar>
+      <div style={{ display: 'flex' }}>
+        <NavbarToggler onClick={toggle} />
+        <NavbarBrand className="text-list-container" href="/">
+          Мониторинг Строительства
+        </NavbarBrand>
+      </div>
+
+      <div style={{ display: 'flex' }}>
+        <NavbarBrand>
+          <Input
+            id="exampleFind"
+            name="findDoc"
+            placeholder="🔍 Поиск"
+            type="text"
+          />
+        </NavbarBrand>
+        <NavbarBrand href="/">
+          <Input
+            id="exampleDate"
+            name="dateDoc"
+            placeholder="date placeholder"
+            type="date"
+          />
+        </NavbarBrand>
+        <NavbarBrand href="/">
+          <Button color="primary">🔬 Фильтры</Button>
+        </NavbarBrand>
+        <NavbarBrand href="/">
+          <CardImg
+            className="avatar-list-container"
+            alt="Avatar"
+            src="https://kvartira.sibir-monitoring.ru/img/user.png"
+          />
+        </NavbarBrand>
+      </div>
+      <Collapse isOpen={isOpen} navbar className="justify-content-end">
         <Nav className="me-auto" navbar>
           <NavItem>
             <NavLink href="/">Выпадает Раз</NavLink>
           </NavItem>
-
         </Nav>
-        <NavbarText>Выпадает Два</NavbarText>
+        <NavLink href="/">Выпадает Два</NavLink>
       </Collapse>
     </Navbar>
-
   );
 }
