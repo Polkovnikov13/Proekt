369ca38be8ceb6d84781781c5fd5caba6f177309
Artem Polkovnikov <artem.polkovnikov93@yandex.ru>
2024-12-09 14:40:09 +0300

    R4

--- server/utils/changeUrl.js
+++ server/utils/changeUrl.js
@@ -5,19 +5,23 @@ async function parseURL(url) {
     const response = await axios.get(url);
     const html = response.data;
 
+    // console.log('Fetched HTML:', html); // Логирование содержимого HTML
+
     const regex = /var\s+n_url\s*=\s*"(.*?)";/;
     const match = html.match(regex);
 
     if (match && match[1]) {
-      return match[1];
+      return match[1]; // Возвращаем найденный URL
     } 
-      throw new Error('URL not found in the HTML.');
+    
+    throw new Error('URL not found in the HTML.');
     
   } catch (error) {
     console.error('Error fetching or parsing the URL:', error);
     throw error;
   }
 }
-// console.log(parseURL('https://rtsp.me/embed/38ZdhZ8D/').then((r)=>console.log(r,'Gere')))
+
 module.exports = parseURL;
+
 
